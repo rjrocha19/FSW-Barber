18fc261e97762eaab40a7a321b507ef45fe1a207
rjrocha19 <rodolfo_rj@live.com>
2024-08-27 16:39:30 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -34,15 +35,24 @@ export default function Home() {
         {/* scheduling */}
         <div>
           <Card className="mt-6">
-            <CardContent className="flex">
+            <CardContent className="flex justify-between p-0">
               {/* Left */}
-              <div className="flex flex-col gap-2 py-5">
-                <Badge>Confirmado</Badge>
-                <h3>Corte de Cabelo</h3>
-                
-                <div className="flex item-center">
-                  
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit">Confirmado</Badge>
+                <h3 className="font-semibold">Corte de Cabelo</h3>
+
+                <div className="flex item-center gap-2">
+                  <Avatar className="w-6 h-6">
+                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                  </Avatar>
+                  <p className="text-sm">Vintage Barber</p>
                 </div>
+              </div>
+              {/* Right */}
+              <div className="flex flex-col justify-center items-center border-l-2 border-solid px-5">
+                <p className="text-sm">Agosto</p>
+                <p className="text-2xl">26</p>
+                <p className="text-sm">20:00</p>
               </div>
             </CardContent>
           </Card>
