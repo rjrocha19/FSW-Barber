3b628898e0d18d2b41f601b855ef00687bfabe1f
rjrocha19 <rodolfo_rj@live.com>
2024-09-08 00:01:28 -0300

    feat: create and update barbershop page

---
+++ app/barbershop/page.tsx
@@ -0,0 +1,28 @@
+import { db } from "../_lib/prisma"
+
+interface BarbershopItemProps {
+  searchParams: {
+    search?: string
+  }
+}
+
+export default async function BarbershopPage({
+  searchParams,
+}: BarbershopItemProps) {
+  const barbershop = await db.barbershop.findMany({
+    where: {
+      name: {
+        contains: searchParams?.search,
+        mode: "insensitive",
+      },
+    },
+  })
+  return (
+    <div>
+      <h2 className="mb-3 mt-6 text-xs font-bold upercase text-gray-400">
+        Resultados para &quot;{searchParams?.search}&quot;
+      </h2>
+      <div className="grid grid-cols-2 gap-4"></div>
+    </div>
+  )
+}
